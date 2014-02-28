620b276d27ff731f07ec651877c81cf023b6fba3
jramirez <jramirez@chicagotechacademy.org>
2014-02-28 14:52:15 -0600

    init commit

---
+++ team_warframe/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamWarframe::Application.config.secret_token = '3b62cff2d39a657b49038e5e99f48ed3f2e7bce0d22ec843c762c2e236cc3aa42a23d8f3bf6baab2dfa1c1ced302b183c463d959b1254af08cc6f67b8b02f1f9'
--- team_warframe/config/routes.rb
+++ team_warframe/config/routes.rb
@@ -1,10 +1,4 @@
-SecondApp2::Application.routes.draw do
-  resources :miniposts
-
-
-  resources :users
-
-
+TeamWarframe::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
